1d1cfb9706a6380d1f5b4e06f741535a2ed97bd0
Adam Hoffman <wintros@yahoo.com>
2020-06-22 14:14:26 -0600

    break time

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="Mikenna">Mikenna Hoffman: 11 PA, 5 Walks, 8 SBs, Hustle all day</p>
+      <p className="Reese">Reese Wagnon: 13 PA, 1 K, MMs from squaring one up, focused</p>
+      <p className="Sierra">Sierra Sikitch: 18 PA, 10 HHB, 7 RBI, 4 2out RBI, crushes the softball</p>
+      <p className="Annika">Annika Spilde: 10 PA, 2 ks, 4 Walks, Great chatter and upbeat</p>
+      <p className="Brianna">Brianna Williams: Leads team in BA, SLG, Best running catch of season, Brushes off mistakes, calm</p>
+      <p className="Jai">Jai McNeal: 438 pitches thrown, 57% strikes, Weak hits 68%, 4.46 FIP, doesn't get down on teammates</p>
+      <p className="Jacey">Jacey Lane: 15 PA, 2 ks, 2 Doubles, Leads team in ROE, Clutch, Most positive person I've met in my life</p>
+      <p className="Maggie">Maggie Philpott: 50% strikes, 7.9 FIP,  Doesn't get distracted</p>
+      <p className="Roxie">Roxie Albert: Gives it her all every game</p>
+      <p className="Avery">Avery Coover: Burning desire to win</p>
+      <p className="Katie">Katie Duncan: Great positive chatter</p>
+      <p className="Charlotte">Charlotte Commons: Keeps her head in the game</p>
+      <p className="Caitlin">Caitlin Anderson: Competitve fire</p>
     </div>
   );
 }
